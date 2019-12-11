998599faf2e4a1d5410d41f82906a02d4af451bc
xukang <xukang@chukong-inc.com>
2019-12-11 10:23:10 +0800

    update

--- get_account_by_name.py
+++ get_account_by_name.py
@@ -3,26 +3,29 @@ from utils import *
 
 def on_query_account(event):
     account_name = textInput.GetValue()
-    print('account_name: {}'.format(account_name))
-    req_data = {
-        "method": "get_account_by_name",
-        "params": [account_name],
-        "id":1
-    }
-    status, result = request_post(req_data)
-    if status:
-        dynamic_global_properties = json_dumps(result)
-        textShow.Clear()
-        textShow.AppendText(dynamic_global_properties)
+    showText = ""
+    if account_name != "":
+        req_data = {
+            "method": "get_account_by_name",
+            "params": [account_name],
+            "id":1
+        }
+        showText = request_post(req_data)
+        if showText == 'null':
+            showText = '账户 {} 不存在'.format(account_name)
+    else:
+        showText = "account name is empty"
+    textShow.Clear()
+    textShow.AppendText(showText)
     queryButton.SetLabel("账户查询")
 
 app = wx.App()
-win = wx.Frame(None, title = "查询测试工具", size=(410,335))
+win = wx.Frame(None, title = "查询测试工具", size=(1000, 800))
 win.Show()
  
-queryButton = wx.Button(win, label = '账户查询',pos = (225,5), size = (80,25))
-textInput = wx.TextCtrl(win, pos = (5,5), size = (210,25))
-textShow = wx.TextCtrl(win, pos = (5,35), size = (390,260), style = wx.TE_MULTILINE | wx.HSCROLL)
+queryButton = wx.Button(win, label = '账户查询',pos = (225, 5), size = (80, 25))
+textInput = wx.TextCtrl(win, pos = (5, 5), size = (210, 25))
+textShow = wx.TextCtrl(win, pos = (5, 35), size = (800, 600), style = wx.TE_MULTILINE | wx.HSCROLL)
 app.Bind(wx.EVT_BUTTON, on_query_account, queryButton)
 
 app.MainLoop()
--- utils.py
+++ utils.py
@@ -9,9 +9,7 @@ headers = {"content-type": "application/json"}
 def request_post(req_data={}):
     response = json.loads(requests.post(node_rpc_url, data = json.dumps(req_data), headers = headers).text)
     print('>> {} {}\n{}\n'.format(req_data['method'], req_data['params'], response))
-    if 'error' in response:
-        return False, response['error']
-    return True, response['result']
+    return json_dumps(response['result'])
 
 def json_dumps(json_data):
     return json.dumps(json_data, indent=4)
